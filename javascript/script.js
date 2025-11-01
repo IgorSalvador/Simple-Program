c55f32acce8e84562a6d1dd2ea8d061fd9abdaf5
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2025-11-01 16:17:26 -0300

    Criando landing page do projeto básica para deploy através do github pages

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    document.getElementById("leadForm").style.display = "none";
+    document.getElementById("feedback").style.display = "block";
+}
